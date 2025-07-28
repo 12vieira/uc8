6d99d51bd82245e93b85df4cfbcc379f626306fd
MATHEUS PIMENTEL VIEIRA <a92263706@rn.senac.local>
2025-07-28 20:56:42 -0300

    aula pressables inicio

---
+++ contatar/constants/data.js
@@ -0,0 +1,27 @@
+export const contactList = [
+    {
+        id: '1',
+        name:'pikachu',
+        type: 'Eletrico',
+        evolution: 'Raichu',
+        photo: require('../assets/images/pikachu.png'),
+        info: 'Não se dá bem com pokemons aquáticos.'
+    },
+    {
+        id: '2',
+        name:'Bulbasaur',
+        type: 'Planta/Venenoso',
+        evolution: 'Ivysaur',
+        photo: require('../assets/images/bulbasaur.png'),
+        info: 'Não se dá bem com pokemons tipo fogo, e é um ótimo lider em grupo.'
+    },
+    {
+        id: '3',
+        name:'Charizard',
+        type: 'Fogo/Voador',
+        evolution: 'Sem evolução',
+        photo: require('../assets/images/charizard.png'),
+        info: 'Teimoso, se acha o dono do pedaço, fraco contra tipo água.'
+    }
+
+]
